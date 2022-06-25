9fd8cfbb779b2b3e4a6f3ed926d14b8e4e819753
codeakki <codewithakki@gmail.com>
2022-06-25 18:52:47 +0530

    inital_Dev

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {LemonButton} from '@posthog/lemon-ui'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey </h1>
+      <LemonButton>
+      Click me LETS Goo Kanika 
+   </LemonButton>
+    </div>
+  );
+}
+
+export default App;
